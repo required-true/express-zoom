1f524506222f965f06abd034b0b5448c65067b17
required-true <lionjk92@naver.com>
2023-03-02 14:29:13 +0900

    Set ws on express and connect

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+import http from 'http'
 import express from 'express'
+import WebSocket from 'ws'
 
 const app = express()
 
@@ -9,5 +11,14 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.render('home'))
 app.get('/*', (req, res) => res.redirect('/'))
 
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+const handleConnection = (socket) => {
+  console.log(socket)
+}
+wss.on('connection', handleConnection)
+
 const handleListen = () => console.log('Listening on http://localhost:3000')
-app.listen(3000, handleListen)
+server.listen(3000, handleListen)
