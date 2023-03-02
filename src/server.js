fb1cc96a303f876734f8cc0d7d721abbd3d1c8f0
required-true <lionjk92@naver.com>
2023-03-02 14:41:48 +0900

    Message sending example

--- src/server.js
+++ src/server.js
@@ -11,14 +11,16 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.render('home'))
 app.get('/*', (req, res) => res.redirect('/'))
 
-
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
 
-const handleConnection = (socket) => {
-  console.log(socket)
-}
-wss.on('connection', handleConnection)
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser ✅')
+  socket.on('close', () => (console.log('Disconnected from Browser ❌')))
+  socket.on('message', (message) => (console.log(message.toString('utf-8'))))
+  socket.send('Hello!!!')
+})
 
-const handleListen = () => console.log('Listening on http://localhost:3000')
-server.listen(3000, handleListen)
+server.listen(3000, () => {
+  console.log('Listening on http://localhost:3000')
+})
