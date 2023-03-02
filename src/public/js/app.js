02b93d8be3dc4d16e737adfdd491097c46c50770
required-true <lionjk92@naver.com>
2023-03-02 16:43:36 +0900

    Room(message, notification) and nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,58 @@
 const socket = io()
 
-const welcome = document.querySelector('#welcome')
-const form = welcome.querySelector('form')
+const welcome = document.getElementById('welcome')
+const welcomeForm = welcome.querySelector('form')
+const room = document.getElementById('room')
 
-form.addEventListener('submit', (event) => {
+room.hidden = true
+
+let roomName;
+
+welcomeForm.addEventListener('submit', handleRoomSubmit)
+
+function handleRoomSubmit(event) {
+  event.preventDefault();
+  const nameInput = welcomeForm.querySelector("#nameInput");
+  const roomInput = welcomeForm.querySelector("#roomInput");
+  socket.emit("enter_room", nameInput.value, roomInput.value, showRoom);
+  roomName = roomInput.value;
+  nameInput.value = '';
+  roomInput.value = '';
+}
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+
+  const msgForm = room.querySelector('#msg')
+  msgForm.addEventListener('submit', handleMessageSubmit)
+}
+
+function addMessage(message) {
+  const ul = room.querySelector('ul')
+  const li = document.createElement('li')
+  li.innerText = message
+  ul.appendChild(li)
+}
+
+function handleMessageSubmit(event) {
   event.preventDefault()
-  const input = form.querySelector('input')
-  socket.emit("enter_room", input.value, (msg) => {
-    console.log(`The backend says: `, msg)
-  });
+  const input = room.querySelector("#msg input");
+  const value = input.value
+  socket.emit('new_message', value, roomName, () => {
+    addMessage(`You: ${value}`)
+   })
   input.value = ''
+}
+
+socket.on('welcome', (user) => {
+  addMessage(`${user} arrived !`)
 })
+
+socket.on('bye', (user) => {
+  addMessage(`${user} left .`)
+})
+
+socket.on('new_message', addMessage)
--- src/server.js
+++ src/server.js
@@ -17,11 +17,25 @@ const httpServer = http.createServer(app)
 const wsServer = SocketIO(httpServer)
 
 wsServer.on('connection', (socket) => {
-  socket.on('enter_room', (roomName, done) => {
-    console.log(roomName)
-    setTimeout(() => {
-      done('Hello from backend')
-    }, 5000);
+
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`)
+  })
+
+  socket.on('enter_room', (nickName, roomName, done) => {
+    socket['nickname'] = nickName
+    socket.join(roomName)
+    done()
+    socket.to(roomName).emit('welcome', socket.nickname)
+  })
+
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`)
+    done()
+  })
+
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((roomName) => socket.to(roomName).emit('bye', socket.nickname))
   })
 })
 
