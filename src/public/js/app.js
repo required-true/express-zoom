d78107d82b1014ea52ab09373d87cfeec901329c
required-true <lionjk92@gmail.com>
2023-03-04 22:11:46 +0900

    Data channel example

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,6 +14,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
   try {
@@ -114,6 +115,9 @@ welcomeForm.addEventListener('submit', handleWelcomeSubmit);
 // Socket Code
 // session 1
 socket.on('welcome', async () => {
+  myDataChannel = myPeerConnection.createDataChannel('chat');
+  myDataChannel.addEventListener('message', console.log);
+  console.log('made data channel');
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log('sent the offer');
@@ -122,6 +126,10 @@ socket.on('welcome', async () => {
 
 // session 2
 socket.on('offer', async (offer) => {
+  myPeerConnection.addEventListener('datachannel', (event) => {
+    myDataChannel = event.channel;
+    myDataChannel.addEventListener('message', console.log);
+  });
   console.log('received the offer');
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
